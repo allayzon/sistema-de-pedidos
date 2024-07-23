895b480ea2dec3a4b731cd6683b560c356c14c44
Allyson <allyson.g.d.santos@gmail.com>
2024-07-23 01:25:19 -0300

    Desenvolvimento do domínio 'Pedido' - 1

--- src/main/java/br/com/sistema_de_pedidos/sistema_de_pedidos/domain/Pedido.java
+++ src/main/java/br/com/sistema_de_pedidos/sistema_de_pedidos/domain/Pedido.java
@@ -3,6 +3,20 @@ package br.com.sistema_de_pedidos.sistema_de_pedidos.domain;
 public class Pedido {
 
     private String idPedido;
+    private int numeroPedido;
     private PedidoStatus statusPedido;
 
+    public Pedido() {
+    }
+
+    public Pedido(String idPedido, int numeroPedido, PedidoStatus statusPedido) {
+        this.idPedido = idPedido;
+        this.numeroPedido = numeroPedido;
+        this.statusPedido = statusPedido;
+    }
+
+    public Pedido(int numeroPedido, PedidoStatus statusPedido) {
+        this.numeroPedido = numeroPedido;
+        this.statusPedido = statusPedido;
+    }
 }
