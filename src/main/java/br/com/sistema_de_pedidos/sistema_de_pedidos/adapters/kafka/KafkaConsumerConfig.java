24c737d816573a30d3e01ddea31630ca8ff92167
Allyson <allyson.g.d.santos@gmail.com>
2024-08-08 00:57:34 -0300

    Limpeza de código na classe Pedido.

--- src/main/java/br/com/sistema_de_pedidos/sistema_de_pedidos/adapters/kafka/KafkaConsumerConfig.java
+++ src/main/java/br/com/sistema_de_pedidos/sistema_de_pedidos/adapters/kafka/KafkaConsumerConfig.java
@@ -1,14 +1,15 @@
 package br.com.sistema_de_pedidos.sistema_de_pedidos.adapters.kafka;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
-import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -20,10 +21,13 @@ public class KafkaConsumerConfig {
     @Bean
     public ConsumerFactory<String, Object> consumerFactory() {
         Map<String, Object> props = new HashMap<>();
-        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:29092");
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "pedido_group");
-        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringSerializer.class);
-        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ErrorHandlingDeserializer.class);
+        props.put(ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS, JsonDeserializer.class.getName());
+        props.put(JsonDeserializer.TRUSTED_PACKAGES, "*");
+        props.put(JsonDeserializer.VALUE_DEFAULT_TYPE, "br.com.sistema_de_pedidos.sistema_de_pedidos.domain.Pedido");
         return new DefaultKafkaConsumerFactory<>(props);
     }
 
--- src/main/java/br/com/sistema_de_pedidos/sistema_de_pedidos/domain/Pedido.java
+++ src/main/java/br/com/sistema_de_pedidos/sistema_de_pedidos/domain/Pedido.java
@@ -1,7 +1,12 @@
 package br.com.sistema_de_pedidos.sistema_de_pedidos.domain;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "pedidos")
 public class Pedido {
 
+    @Id
     private String idPedido;
     private PedidoStatus statusPedido;
 
