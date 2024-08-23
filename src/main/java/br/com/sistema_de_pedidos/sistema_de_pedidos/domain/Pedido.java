100a861c5a244ab4959bb865c0fd778e59cbf11b
Allyson <allyson.g.d.santos@gmail.com>
2024-08-22 23:48:18 -0300

    Limpeza de código na classe Pedido.

--- src/main/java/br/com/sistema_de_pedidos/sistema_de_pedidos/domain/Pedido.java
+++ src/main/java/br/com/sistema_de_pedidos/sistema_de_pedidos/domain/Pedido.java
@@ -10,18 +10,6 @@ public class Pedido {
     private String idPedido;
     private PedidoStatus statusPedido;
 
-    public Pedido() {
-    }
-
-    public Pedido(String idPedido, PedidoStatus statusPedido) {
-        this.idPedido = idPedido;
-        this.statusPedido = statusPedido;
-    }
-
-    public Pedido(PedidoStatus statusPedido) {
-        this.statusPedido = statusPedido;
-    }
-
     public String getIdPedido() { return idPedido; }
 
     public PedidoStatus getStatusPedido() { return statusPedido; }
